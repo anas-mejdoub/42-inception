d9ea915218647fe73983a62bde25a27b9e51d7e7
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-12-16 10:45:14 -0500

    ftp works fine now

--- src/requirements/bonus/ftp/tools/setup_ftp.sh
+++ src/requirements/bonus/ftp/tools/setup_ftp.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-useradd /home/${FTP_USER}
+useradd -m anas
 mkdir -p  /var/run/vsftpd/empty
-echo "${FTP_USER}:${FTP_PASSWD}" | chpasswd
 
-usermod -d /var/www/html/ ${FTP_USER}
 
+usermod -d /var/www/html/ anas
+
+echo "anas:anas" | chpasswd
+chown -R anas:anas /var/www/html/
+chmod -R 755 /var/www/html
 echo "ftp is starting..."
 
 exec $@
