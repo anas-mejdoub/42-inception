2f06f4fd5709c9a288214f9c3ec114b35f5120fd
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-12-27 06:00:14 -0500

    works

--- src/requirements/bonus/static-website/tools/vite.config.js
+++ src/requirements/bonus/static-website/tools/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+	server: {
+    host: '0.0.0.0',  // Listen on all interfaces
+    port: 5173         // Ensure it's listening on port 5173
+  }
 })
