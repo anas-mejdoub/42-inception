74293fda7cc0b216b31a96a7dd4f5e91633accf5
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-12-27 09:58:50 -0500

    8080 adminer

--- src/requirements/bonus/adminer/tools/setup_adminer.sh
+++ src/requirements/bonus/adminer/tools/setup_adminer.sh
@@ -1,7 +1,12 @@
 #!bin/bash
 
-curl -L -o /var/www/html/adminer.php https://www.adminer.org/latest.php
+rm -f /var/www/html/index.php
+
+curl -L -o /var/www/html/index.php https://www.adminer.org/latest.php
 
 cd /var/www/html
+
+
+#mv adminer.php index.php
 
 exec php -S 0.0.0.0:8080 -t /var/www/html/
