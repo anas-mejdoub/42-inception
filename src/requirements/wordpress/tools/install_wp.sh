e524b76113ee35b8288a0b8c774ece709a69d9dd
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-12-30 14:36:43 -0500

    sh

--- src/requirements/wordpress/tools/install_wp.sh
+++ src/requirements/wordpress/tools/install_wp.sh
@@ -2,19 +2,17 @@
 
 echo "waiting for mariadb.."
 sleep 10
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+#curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+#chmod +x wp-cli.phar
+#mv wp-cli.phar /usr/local/bin/wp
 if [ -f "/var/www/html/wp-config.php" ]; then
 	echo "wp is already installed !"
 	echo "php FPM is starting now"
-	sleep 6
-	cd /var/www/html
-	chown -R www-data:www-data .
+#	chown -R www-data:www-data .
 	exec php-fpm8.2 -F
 fi
 
-if [ ! -d "/usr/local/bin/wp" ]; then 
+if [ ! -f "/usr/local/bin/wp" ]; then 
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
@@ -32,12 +30,12 @@ wp core download --allow-root
 wp config create --dbname=$WP_DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWD --dbhost=mariadb:3306 --allow-root
 wp config set WP_REDIS_HOST 'redis' --type=constant --allow-root
 wp config set WP_REDIS_PORT 6379 --type=constant --raw --allow-root
-wp user create $SEC_USER_NAME $SEC_USER_EMAIL --role=editor --user_pass=$SEC_USER_PASS
+wp user create $SEC_USER_NAME $SEC_USER_EMAIL --role=editor --user_pass=$SEC_USER_PASS --allow-root
 wp core install  --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
 wp plugin install redis-cache --activate --allow-root
 wp redis enable --allow-root
-wp theme install twentytwentythree
-wp theme activate twentytwentythree
+wp theme install twentytwentyfour --allow-root
+wp theme activate twentytwentyfour --allow-root
 chmod -R 777 /var/www/html/*
 chown -R www-data:www-data .
 echo "php FPM is strating now" 
