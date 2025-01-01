cadafd1a21c1e03f2381cc10f910b4b610a3be39
anas-mejdoub <anasmejdoub2000@gmail.com>
2025-01-01 12:43:53 +0100

    mariadb

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -8,12 +8,10 @@ if [ ! -d "/var/lib/mysql/${WP_DB_NAME}" ]; then
 	cat << EOF > /tmp/init_mariadb.sql
 USE mysql;
 FLUSH PRIVILEGES;
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASS}';
-CREATE DATABASE ${WP_DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE ${WP_DB_NAME} IF NOT EXISTS;
 CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASSWD}';
 GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${USER_PASS}';
-
 FLUSH PRIVILEGES;
 EOF
 	mariadbd --user=mysql --bootstrap < /tmp/init_mariadb.sql
