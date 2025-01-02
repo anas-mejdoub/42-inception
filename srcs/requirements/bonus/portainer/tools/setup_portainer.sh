60dfdf5d7279069fbc2d30bbbc45339492892c37
anas-mejdoub <anasmejdoub2000@gmail.com>
2025-01-02 15:27:12 +0100

    portainer

--- srcs/requirements/bonus/portainer/tools/setup_portainer.sh
+++ srcs/requirements/bonus/portainer/tools/setup_portainer.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# download the portainer
-curl -LO https:/github.com/portainer/portainer/releases/download/2.19.2/portainer-2.19.2-linux-amd64.tar.gz
 
-# extract the portainer 
 
 tar -xvf portainer-2.19.2-linux-amd64.tar.gz
 cd portainer
-#ls -l
-#cat portainer
-#yes > /dev/null
-# move it to a excutable path and make the binary executable
-#mv portainer /bin/portainer
 
 chmod +x portainer
 
